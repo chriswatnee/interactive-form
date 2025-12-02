955b004bb78fc7f274924f7a7dc817bc36d634d2
Chris Watnee <chriswatnee@users.noreply.github.com>
2025-12-02 15:41:06 -0500

    Display/hide the other job role input based on the title selection

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 const nameInput = document.querySelector("#name");
+const titleSelect = document.querySelector("#title");
+const otherJobRoleInput = document.querySelector("#other-job-role");
 
 // Set focus on name input field
 nameInput.focus();
+
+// Hide other job role input field
+otherJobRoleInput.style.display = "none";
+
+// Listen for title select change
+titleSelect.addEventListener("change", () => {
+  // Display/hide the other job role input based on the selection
+  if (titleSelect.value === "other") {
+    otherJobRoleInput.style.display = "inherit";
+  } else {
+    otherJobRoleInput.style.display = "none";
+  }
+});
