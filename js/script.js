11b87e9394114afe3b15ddd4b0f88db19be25ef0
Chris Watnee <chriswatnee@users.noreply.github.com>
2025-12-03 09:28:19 -0500

    Update color select based on the t-shirt design selection

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const nameInput = document.querySelector("#name");
 const titleSelect = document.querySelector("#title");
 const otherJobRoleInput = document.querySelector("#other-job-role");
+const designSelect = document.querySelector("#design");
+const colorSelect = document.querySelector("#color");
+const colorOptions = colorSelect.querySelectorAll("option[data-theme]");
 
 // Set focus on name input field
 nameInput.focus();
@@ -15,5 +18,28 @@ titleSelect.addEventListener("change", () => {
     otherJobRoleInput.style.display = "inherit";
   } else {
     otherJobRoleInput.style.display = "none";
+  }
+});
+
+// Disable color select
+colorSelect.disabled = true;
+
+// Listen for design select change
+designSelect.addEventListener("change", () => {
+  // Enable color select
+  colorSelect.disabled = false;
+  // Select first color option for design
+  const colorOption = colorSelect.querySelector(`option[data-theme="${designSelect.value}"]`);
+  colorOption.selected = true;
+  // Hide/show and disable/enable color options
+  for (let i = 0; i < colorOptions.length; i++) {
+    const colorOption = colorOptions[i];
+    if (designSelect.value === colorOption.getAttribute("data-theme")) {
+      colorOption.hidden = false;
+      colorOption.disabled = false;
+    } else {
+      colorOption.hidden = true;
+      colorOption.disabled = true;
+    }
   }
 });
